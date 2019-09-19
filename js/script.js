0d0e2e360780c44c7c14c084e190f16bdd43e74e
Daniel Minschinski <danielminschinski@gmail.com>
2019-09-19 15:23:57 +0200

    Add section vanillajs and section jquery, style scss and code script

---
+++ js/script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+(function(){
+	var accordionVanillaJS = function(){
+		var headers = document.getSelectorAll('#accordion-vanillajs h3');
+		var headerClickCallback = function(){
+			this.classList.add('active');
+			for(var i = 0; i < headers.length; i++){
+				if(headers[i].classList.contains('active') && headers[i] != this){
+					headers[i].classList.remove('active');
+				}
+			}
+		}
+	}
+
+
+
+
+
+
+
+
+
+})();
